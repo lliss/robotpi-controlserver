691c13038ccc8a696f6e690a173857080c8c8892
lliss <llisscommunications@gmail.com>
2013-05-05 15:00:29 -0400

    commenting code

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+/**
+ * @file
+ * The control interface for the Raspberry Pi robot.
+ * Server listens on 8080 and returns static files in the 'public directory'.
+ */
+
 var io = require('socket.io'),
     connect = require('connect'),
     http = require('http');
+
+// Set up the control interface server on 8080.
 var controls = connect().use(connect.static('public')).listen(8080);
 
 var responder = io.listen(controls);
@@ -9,6 +17,8 @@ responder.sockets.on('connection', function (socket) {
   socket.emit('entrance', {message: 'Controller Engaged'});
 
   socket.on('action', function (data) {
+    // When we get an 'action' event parse out the value and send it to the
+    // control API as a PUT.
     var requestAction = JSON.parse(data).value;
     console.log(requestAction);
     var options = {
